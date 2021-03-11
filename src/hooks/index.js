d38d7f3aa7fe58f547b147d8788a7f52afd013fe
Mateusz Flisikowski <contact@mflisikowski.dev>
2021-03-11 21:30:47 +0100

    add hooks: useEventListener, useScrollLock

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,4 @@
+export { default as useEventListener } from './useEventListener';
 export { default as useLocalStorage } from './useLocalStorage';
+export { default as useScrollLock } from './useScrollLock';
 export { default as useTheme, getTheme } from './useTheme';
