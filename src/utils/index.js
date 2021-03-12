aa3a441e3a78948f7e799b408a6bbf273c25a5c8
Mateusz Flisikowski <contact@mflisikowski.dev>
2021-03-12 07:35:15 +0100

    add utils fn stringIncludesHTML

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,9 @@
 export { default as isExternalURL } from './isExternalURL';
 
+export const SIZE_HERO = '860x580';
 export const SIZE_230_170 = '230x170';
 export const SIZE_150_150 = '150x150';
 
-export const imageAcceptableFormats = [SIZE_230_170, SIZE_150_150];
+export const imageAcceptableFormats = [SIZE_230_170, SIZE_150_150, SIZE_HERO];
+
+export const stringIncludesHTML = (s) => s.match(/<[^>]+>/g);
