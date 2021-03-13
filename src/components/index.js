2847b5a8b4c5b5ff8f748594975620014c801cf1
Mateusz Flisikowski <contact@mflisikowski.dev>
2021-03-13 20:41:21 +0100

    move components outside the project: Image and ImageBackground

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 export { default as ImagePlaceholder } from './multimedia/ImagePlaceholder';
-export { default as ImageBackground } from './multimedia/ImageBackground';
 export { default as ImageLightbox } from './multimedia/ImageLightbox';
-export { default as Image } from './multimedia/Image';
 export { default as Video } from './multimedia/Video';
 
 export { default as MapLeaflet } from './MapLeaflet';
