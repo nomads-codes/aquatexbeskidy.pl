20c1a14e86d111d597f75dd3ba9187eb51c344d3
Mateusz Flisikowski <contact@mflisikowski.dev>
2021-03-23 22:47:28 +0100

    cleanup breakpoints...

--- src/theme/_breakpoints.js
+++ src/theme/_breakpoints.js
@@ -2,14 +2,16 @@
 // Breakpoints
 // ─────────────────────────────────────────────────────────────────────────────
 
-export const mobile_small = '320';
-export const mobile_base = '375';
-export const mobile_big = '425';
-export const tablet_base = '768';
-export const tablet_big = '1024';
-export const desktop_small = '1200';
-export const desktop_base = '1440';
-export const desktop_big = '1680';
+export const mobile_small = 320;
+export const mobile_base = 375;
+export const mobile_big = 425;
+export const tablet_base = 768;
+export const tablet_big = 1024;
+export const desktop_small = 1200;
+export const desktop_base = 1440;
+export const desktop_big = 1680;
+
+const calcMax = (breakpoint) => breakpoint - 1;
 
 const breakpoints = {
   min: {
@@ -23,14 +25,14 @@ const breakpoints = {
     desktop_big: `@media (min-width: ${desktop_big}px)`,
   },
   max: {
-    mobile_small: `@media (max-width: ${parseInt(mobile_small - 1)}px)`,
-    mobile_base: `@media (max-width: ${parseInt(mobile_base - 1)}px)`,
-    mobile_big: `@media (max-width: ${parseInt(mobile_big - 1)}px)`,
-    tablet_base: `@media (max-width: ${parseInt(tablet_base - 1)}px)`,
-    tablet_big: `@media (max-width: ${parseInt(tablet_big - 1)}px)`,
-    desktop_small: `@media (max-width: ${parseInt(desktop_small - 1)}px)`,
-    desktop_base: `@media (max-width: ${parseInt(desktop_base - 1)}px)`,
-    desktop_big: `@media (max-width: ${parseInt(desktop_big - 1)}px)`,
+    mobile_small: `@media (max-width: ${calcMax(mobile_small)}px)`,
+    mobile_base: `@media (max-width: ${calcMax(mobile_base)}px)`,
+    mobile_big: `@media (max-width: ${calcMax(mobile_big)}px)`,
+    tablet_base: `@media (max-width: ${calcMax(tablet_base)}px)`,
+    tablet_big: `@media (max-width: ${calcMax(tablet_big)}px)`,
+    desktop_small: `@media (max-width: ${calcMax(desktop_small)}px)`,
+    desktop_base: `@media (max-width: ${calcMax(desktop_base)}px)`,
+    desktop_big: `@media (max-width: ${calcMax(desktop_big)}px)`,
   },
 };
 
